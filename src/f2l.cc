2065ab0427ed648e60aa3d921e40708092c0b540
ljf12825linux <ljf12825@graingen.com>
2026-07-07 23:09:53 +0800

    Feat: introducing ggclirouter and f2l as main

--- src/f2l.cc
+++ src/f2l.cc
@@ -1,36 +1,16 @@
-// f2l
+#include "ggclirouter.h"
+#include "commands/init.h"
 #include <iostream>
-#include <cstring>
-#include <string>
-#include <vector>
-
-class ArgParser {
-    std::vector<std::string> args;
-public:
-    ArgParser(int argc, char* argv[]) {
-        args.assign(argv + 1, argv + argc);
-    }
-
-    bool has(const std::string& option) {
-        for (const auto& arg : args) {
-            if (arg == option) return true;
-        }
-        return false;
-    }
-};
 
 int main(int argc, char* argv[]) {
-    ArgParser parser(argc, argv);
+    CLIRouter router;
 
-    if (parser.has("-v") || parser.has("--version")) {
-        std::cout << "f2l(Fetch to Local) version 0.1.0" << std::endl;
-        return 0;
-    }
+    router.reg_cmd("init", handle_init);
 
-    if (parser.has("-h") || parser.has("--help")) {
-        std:: cout << "Help Info" << std::endl;
-        return 0;
-    }
+    router.set_error_handler([](const std::string& msg) {
+            std::cerr << "[f2l error] " << msg << "\n";
+            });
 
+    router.dispatch(argc, argv);
     return 0;
 }
