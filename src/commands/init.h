3ece3540e21b8ac3ef34fb230720dd2c60004a84
ljf12825linux <ljf12825@graingen.com>
2026-07-08 16:46:12 +0800

    Fix[init.h]: add missing function signature

--- src/commands/init.h
+++ src/commands/init.h
@@ -2,4 +2,6 @@
 #include <vector>
 #include <string>
 
+void init_local_workspace(const std::string& platform, const std::string& problem_id);
+
 void handle_init(const std::vector<std::string>& args);
